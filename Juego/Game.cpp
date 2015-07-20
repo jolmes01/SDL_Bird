0f202fa3f6ea78deb08f8ad51abc09bf10b4cafa
eddfenix13 <edd_fenix13@hotmail.com>
2015-07-19 22:51:59 -0500

    Clonaciion

--- Juego/Game.cpp
+++ Juego/Game.cpp
@@ -2,26 +2,17 @@
 /*
 	Implementación de la clase Game. 
 	En este archivo se incluye toda la lógica de nuestro programa. 
-	Es aquí donde se ubica el código para la creación y trazado de las figuras.
 */
 
 #include "Game.h"
+#include "data.h"
+#include "SocketDatagrama.h"
 
-#define aleatorio(min, max) min+rand()%max
-#define MAX_OFFSET 300
-#define CONT_NUEVO 1
-#define MAX_DIA 100
+/* Global functions */
 
-int contador = 0;
 
-int* fueraRandom(int , int);
-
-Game::Game(int ancho, int alto, int inicial){ //Se cambio el ingreso de ancho y alto de init al constructor de la clase ya que se necesita antes
-	_ancho = ancho;
-	_alto = alto;
-	srand(time(NULL));
-	for (int i = inicial; i > 0; i--)
-		agrega();
+Game::Game(){
+	iniciarConexion();
 }
 
 Game::~Game(){
@@ -32,25 +23,53 @@ bool Game::running(){
 	return m_bRunning;
 }
 
-void Game::init(const char* titulo, int xpos, int ypos, int banderas){
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+void Game::init(const char* titulo, int xpos, int ypos, int ancho, int alto){
+	m_bRunning = 1;
+	if (SDL_Init(SDL_INIT_VIDEO) != 0){
 		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
 		m_bRunning = 0;
 	}
-	m_pWindow = SDL_CreateWindow(titulo, xpos, ypos, _ancho, _alto, banderas);
-	if (m_pWindow == nullptr){
+	ventana = SDL_CreateWindow(titulo, xpos, ypos, ancho, alto, SDL_WINDOW_SHOWN);
+	if (ventana == nullptr){
 		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
 		m_bRunning = 0;
 	}
-	m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	render = SDL_CreateRenderer(ventana, -1, SDL_RENDERER_ACCELERATED);
 	if (m_pRenderer == nullptr){
 		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
 		m_bRunning = 0;
 	}
-	m_bRunning = 1;
+	//se carga la imagen que contiene todos los graficos
+	bmp = SDL_LoadBMP("atlas.bmp");
+	//se define el blanco como color transparente
+	SDL_SetColorKey(bmp, 1, SDL_MapRGB(bmp->format, 255, 255, 255));
+	textura = SDL_CreateTextureFromSurface(render, bmp);
+	SDL_FreeSurface(bmp);
+	if (textura == NULL) {
+        std::cout << "FAILED TO FIND THE IMAGE" << std::endl;
+		m_bRunning = 0;
+	}
+
+	SDL_RenderClear(render);
+	renderFondo();
+
+	//Inicializa la posición de los pajaros dentro de la pantalla, pasandolos del atlas.bmp al recuadro del juego
+	initPajarosOrigen();
+	SDL_RenderPresent(render);
 }
 
 void Game::handleEvents(){
+	while (SDL_PollEvent(&event)) {
+			//salto
+			if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_SPACE) {
+				rectangulo_destino[nJugador].y -= 100;
+				angulo = 300;
+				infoToSend.opcode = 1;
+			} else if (event.type == SDL_QUIT) {
+				infoToSend.opcode = CLOSE;
+				done = 0;
+			}
+		}
 	SDL_Event event;
 	if(SDL_PollEvent(&event))
 	{
@@ -66,118 +85,106 @@ void Game::handleEvents(){
 }
 
 void Game::update(){
-	cout<<"Figuras: "<<asteroides.size()<<endl;
-	contador++;
-	if(contador==CONT_NUEVO){
-		agrega();
-		contador = 0;      
-	}
-	if(asteroides.size() > 0){
-		for(std::vector<Asteroide>::iterator i=asteroides.begin(); i != asteroides.end(); i++)
-			if(! (*i).mueve() ) 
-				asteroides.erase(i--);
-			else{
-				if(i>asteroides.begin()){
-					for(std::vector<Asteroide>::iterator j=asteroides.begin(); j < i; j++)
-						//if(((*i).obtieneCentro()-(*j).obtieneCentro()).length()<(*i).obtieneDiametro()/2+(*j).obtieneDiametro()/2){
-						if(((*i).obtieneCentro()-(*j).obtieneCentro()).length()<MAX_DIA){
-							if(((*i).obtieneCentro()-(*j).obtieneCentro()).length()<(*i).obtieneDiametro()/2+(*j).obtieneDiametro()/2){
-								if((*i).obtieneVelocidad()*(*i).obtieneMasaAprox() < (*j).obtieneVelocidad()*(*j).obtieneMasaAprox())
-									asteroides.erase(i);
-								else
-									asteroides.erase(j);
-								i--;
-								j--;
-							}
-							else{
-								vector<Vector2D> puntos =(*i).obtieneVertices();
-								bool salir = false;
-								int veces = 0;
-								Vector2D centro = (*i).obtieneCentro();
-								vector<Vector2D> otrosPuntos =(*j).obtieneVertices();
-								for(std::vector<Vector2D>::iterator k=puntos.begin(); k!=puntos.end(); k++){
-									float d1 = (centro-(*k)).length();
-									for(std::vector<Vector2D>::iterator l=otrosPuntos.begin(); l!=otrosPuntos.end(); l++){
-										float d2 = (centro-(*l)).length();
-										if(d1>d2){
-											veces++;
-											if(veces>4){
-												//cout<<"Centro i: "<<(*i).obtieneCentro().getX()<<","<<(*i).obtieneCentro().getY()<<endl;
-												//cout<<"Punto k: "<<(*k).getX()<<","<<(*k).getY()<<endl;
-												//cout<<"Punto l: "<<(*l).getX()<<","<<(*l).getY()<<endl;
-												//cout<<d1<<","<<d2<<endl<<endl;
-												if((*i).obtieneVelocidad()*(*i).obtieneMasaAprox() < (*j).obtieneVelocidad()*(*j).obtieneMasaAprox()){
-													(*j).cambiaRumbo((*l),(*i).obtieneRumbo()-((*k)-centro));
-													asteroides.erase(i);
-												}
-												else{
-													(*i).cambiaRumbo((*k),(*j).obtieneRumbo()-((*l)-(*j).obtieneCentro()));
-													asteroides.erase(j);
-												}
-												i--;
-												j--;
-												salir = true;
-												break;
-											}
-
-										}
-									}
-									if(salir)
-										break;
-								}
-							}
-						}
-				}
-			}
-		/*std::vector<Asteroide>::iterator i=asteroides.begin();
-		vector<Vector2D> puntos =(*i).obtieneVertices(); 
-			for(std::vector<Vector2D>::iterator k=puntos.begin(); k!=puntos.end(); k++)
-				cout<<"Punto k: "<<(*k).getX()<<","<<(*k).getY()<<endl;
-		*/
-	}
-
 }
 
 void Game::render(){
-	SDL_RenderClear(m_pRenderer);
-	SDL_SetRenderDrawColor(m_pRenderer, 0, 0, 0, 255);
-	if(asteroides.size() > 0)	
-		for(std::vector<Asteroide>::iterator i=asteroides.begin(); i != asteroides.end(); i++){
-			SDL_SetRenderDrawColor(m_pRenderer, (*i).obtieneColor()[0], (*i).obtieneColor()[1], (*i).obtieneColor()[1], 255);
-			SDL_RenderDrawLines(m_pRenderer, (*i).getPoints(), (*i).obtieneN()+1);
-		}
-	SDL_SetRenderDrawColor(m_pRenderer, 0, 0, 0, 255);
-	SDL_RenderPresent(m_pRenderer);
+	
 }
 
 void Game::clean(){
-	SDL_DestroyRenderer(m_pRenderer);
-	SDL_DestroyWindow(m_pWindow);
+	/* Free all objects */
+	SDL_DestroyTexture(textura);
+	SDL_DestroyRenderer(render);
+	SDL_DestroyWindow(ventana);
+
+	/* Quit program */
 	SDL_Quit();
 }
 
-void Game::agrega(){
-	int* lugarAleatorio = new int[2];
-	lugarAleatorio = fueraRandom(_ancho, _alto);
-	Vector2D aux(aleatorio(1, _ancho), aleatorio(1, _alto));
-	asteroides.emplace_back(Asteroide(lugarAleatorio[0], lugarAleatorio[1], aux));
+// se encarga de copiar 3 veces el fondo y presentarlo consecutivamente
+void Game::renderFondo()
+{
+	SDL_Rect	rectangulo_origen;
+	rectangulo_origen.x = 0;
+	rectangulo_origen.y = 0;
+	rectangulo_origen.w = 288;
+	rectangulo_origen.h = 510;
+
+	SDL_Rect	rectangulo_destino;
+	rectangulo_destino.x = 0;
+	rectangulo_destino.y = 0;
+	rectangulo_destino.w = 288;
+	rectangulo_destino.h = 510;
+
+	SDL_RenderCopy(render, textura, &rectangulo_origen, &rectangulo_destino);
+	rectangulo_destino.x += 288;
+	SDL_RenderCopy(render, textura, &rectangulo_origen, &rectangulo_destino);
+	rectangulo_destino.x += 288;
+	SDL_RenderCopy(render, textura, &rectangulo_origen, &rectangulo_destino);
 }
 
-
-int* fueraRandom(int ancho, int alto) {
-	int* coordenada = new int[2];
-	if(rand()%2 == 0)
-		coordenada[0] = rand()%(MAX_OFFSET+ancho);
-	else
-		coordenada[0] = -1*rand()%MAX_OFFSET;
-	if(rand()%2 == 0)
-		if(coordenada[0]>=0 && coordenada[0]<=ancho)
-			coordenada[1] = alto+rand()%MAX_OFFSET;
-		else
-			coordenada[1] = rand()%MAX_OFFSET+alto;
-	else
-		coordenada[1] = -1*rand()%MAX_OFFSET;
-	coordenada[0] += coordenada[0]<-50&&coordenada[0]>ancho+50?0:coordenada[0]<0?-50:50;
-	coordenada[1] += coordenada[1]<-50&&coordenada[1]>alto+50?0:coordenada[1]<0?-50:50;
-	return coordenada;
+void Game::initPajarosOrigen()
+{
+	int xPos[] = {5,175,228};
+	int yPos[] = {982,866};
+	int w = 35, h = 25, i = 0;
+	for (i = 0; i < 3; ++i)
+	{
+		//Rectangulo con las coordenadas donde se encuentra el pagaro en la textura
+		rectangulo_origen[i].x = xPos[i];
+		if(i == 2) rectangulo_origen[i].y = yPos[1];
+		else rectangulo_origen[i].y = yPos[0];
+		rectangulo_origen[i].w = w;
+		rectangulo_origen[i].h = h;
+		//Rectangulo con las coordenadas donde se encuentra el pagaro en la textura
+		rectangulo_destino[i].x = 100 * (i+1);
+		rectangulo_destino[i].y = 300;
+		rectangulo_destino[i].w = w;
+		rectangulo_destino[i].h = h;
+		SDL_RenderCopy(render, textura, &rectangulo_origen[i], &rectangulo_destino[i]);
+	}
 }
+
+void Game::iniciarConexion(){
+	struct birdPackage infoToSend, infoReceived;
+	int	port;
+	char serverIp  [16];
+	if (argc != 3) {
+        std::cout << "USO flappy <serverIp> <port>" << std::endl;
+		exit(-1);
+	}
+
+	strcpy(serverIp, argv[1]);
+	port = atoi(argv[2]);
+
+	SocketDatagrama	socket;
+
+	std::cout << "Enviando al servidor: " << serverIp << " : " << port << " datos iniciados" << std::endl;
+
+	bzero(&infoToSend,sizeof(birdPackage));
+	bzero(&infoReceived,sizeof(birdPackage));
+
+	/* CONEXION POR PRIMERA VEZ */
+	infoToSend.opcode = NEW;
+
+	std::cout << "OPCODE: " << infoToSend.opcode << std::endl;
+
+	PaqueteDatagrama paq((char *)&infoToSend, sizeof(birdPackage), serverIp, port);
+	socket.envia(paq);
+
+    PaqueteDatagrama receive(sizeof(birdPackage));
+    socket.recibe(receive);
+
+    memcpy(&infoReceived, receive.obtieneDatos(), sizeof(birdPackage));
+
+	if(infoReceived.opcode == DENY) //Se rechaza al jugador, por lo que se tiene que salir
+	{
+		std::cout << "Partida llena intente más tarde..." << std::endl;
+		exit(-1);
+	}else{
+		nJugador = infoReceived.jugadorNum;
+		infoToSend.opcode = JUMP;
+		std::cout << "Partida iniciada, jugador: " << nJugador << std::endl;
+	}
+}
+
